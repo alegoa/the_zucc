a96c765fd21f82080a3b686f7f027b2ba4af3187
antoine <antoine.morvant@nicecotedazur.org>
2018-07-16 16:04:10 +0200

    more messages

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,17 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+
+
+    if message.text.include? "Bonjour"
+        message.reply(text: "wesh")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+
+
+    else
+        message.reply(text: 'Hello, human!')
+    end
+
+
 end
