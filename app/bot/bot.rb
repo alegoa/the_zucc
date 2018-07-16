43614f4ea4aa5ce338b9b9fa35bb83845985c48a
antoine <antoine.morvant@nicecotedazur.org>
2018-07-16 16:15:43 +0200

    more messages2

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -11,6 +11,65 @@ Bot.on :message do |message|
         message.reply(text: "wesh")
     elsif message.text.include? "Bonsoir"
         message.reply(text: "wesh ! ")
+    elsif message.text.include? "bonjour"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "bonsoir"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "ça va ?"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "ça va"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "ca va"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "ca va ?"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "salut"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "pd"
+        message.reply(text: "toi pd ! Gros pd !")
+    elsif message.text.include? "nique ta mere"
+        message.reply(text: "mais alors toi nbtm ")
+    elsif message.text.include? "nique ta mère"
+        message.reply(text: "tg ")
+    elsif message.text.include? "tg"
+        message.reply(text: "non ")
+    elsif message.text.include? "kefa"
+        message.reply(text: "R et toi ! ")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "pd"
+        message.reply(text: "toi pd ! Gros pd !")
+    elsif message.text.include? "nique ta mere"
+        message.reply(text: "mais alors toi nbtm ")
+    elsif message.text.include? "ntm"
+        message.reply(text: " nbtm ")
+    elsif message.text.include? "nique ta mère"
+        message.reply(text: "tg ")
+    elsif message.text.include? "tg"
+        message.reply(text: "non ")
+    elsif message.text.include? "kefa"
+        message.reply(text: "R et toi  ")
+    elsif message.text.include? "kefa ?"
+        message.reply(text: "R et toi ")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "pd"
+        message.reply(text: "toi pd ! Gros pd !")
+    elsif message.text.include? "nique ta mere"
+        message.reply(text: "mais alors toi nbtm ")
+    elsif message.text.include? "nique ta mère"
+        message.reply(text: "tg ")
+    elsif message.text.include? "tg"
+        message.reply(text: "non ")
+    elsif message.text.include? "kefa"
+        message.reply(text: "R et toi  ")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+    elsif message.text.include? "Bonsoir"
+        message.reply(text: "wesh ! ")
+        
 
 
     else
